e2112c0bc2f349ea0204f0e04a0e941612bf9cea
Micro-Organism <coolrabbit1993@gmail.com>
2024-11-07 15:46:52 +0800

    feat: springboot 集成 hive

---
+++ zero-hive-boot/src/test/java/com/zero/hive/ZeroHiveBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero.hive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroHiveBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
